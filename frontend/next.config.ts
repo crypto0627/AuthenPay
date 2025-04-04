e946809f58b0cdc1d31aa808c2d72ecdb680d4e6
crypto0627 <jake0627a1@gmail.com>
2025-04-04 22:14:59 +0800

    feat: wallet verify

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    formats: ['image/avif', 'image/webp']
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  }
+}
 
-export default nextConfig;
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+})
+
+export default withPWA(nextConfig)
--- frontend/src/app/web3Provider.tsx
+++ frontend/src/app/web3Provider.tsx
@@ -7,23 +7,27 @@ import {
 } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import {
-  mainnet,
-  avalanche,
-  polygon,
-  base,
   sepolia,
+  avalancheFuji,
+  polygonAmoy,
+  arbitrumSepolia,
+  baseSepolia,
 } from 'wagmi/chains';
 import {
   QueryClientProvider,
   QueryClient,
 } from "@tanstack/react-query";
 
+// Make sure your projectId is valid and properly configured
+// This should be from your WalletConnect Cloud dashboard
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'd958b2135ca20bf2285350e9336b22fc';
+
 const config = getDefaultConfig({
     appName: 'AuthenPay',
-    projectId: 'd958b2135ca20bf2285350e9336b22fc',
-    chains: [mainnet, avalanche, polygon, base, sepolia],
+    projectId: projectId,
+    chains: [baseSepolia, sepolia, avalancheFuji, arbitrumSepolia, polygonAmoy],
     ssr: true,
-  });
+});
 
 const queryClient = new QueryClient();
 
