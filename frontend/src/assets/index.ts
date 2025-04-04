391a19c97cfc2d44ae641d0efb43026d16b09f7a
crypto0627 <jake0627a1@gmail.com>
2025-04-04 21:59:46 +0800

    feat: assets

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,26 @@
+import logo from './logo.svg'
+import streamer from './streamer.png'
+import send from './Send.svg'
+import shield from './Shield.svg'
+import star from './Star.svg'
+import menu from './menu.svg'
+import close from './close.svg'
+import arrowUp from './arrow-up.svg'
+import github from './github.png'
+import people01 from './people01.png'
+import people02 from './people02.png'
+import userFriendly from './user-friendly.svg'
+export {
+  logo,
+  streamer,
+  send,
+  shield,
+  star,
+  menu,
+  close,
+  arrowUp,
+  github,
+  people01,
+  people02,
+  userFriendly
+}
